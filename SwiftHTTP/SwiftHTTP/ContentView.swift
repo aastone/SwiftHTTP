6d927e6d1aecccc5c5577bfb66bd554722956956
aastone <stoneiswang@gmail.com>
2023-02-22 17:15:47 +0800

    Init

---
+++ SwiftHTTP/SwiftHTTP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftHTTP
+//
+//  Created by Yipu Wang on 2/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftHTTP/SwiftHTTP/SwiftHTTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHTTPApp.swift
+//  SwiftHTTP
+//
+//  Created by Yipu Wang on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHTTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
